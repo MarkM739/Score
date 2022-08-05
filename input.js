5f816fb3b32c5ff192e234bb14ffba4d4920113f
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2022-08-05 13:20:53 -0230

    Re-hash Day1
    Simple button, reading the values back to me in console log.
    
    Still trying to get a working listener function to transfer input data into TwoDArrays

---
+++ input.js
@@ -0,0 +1,15 @@
+
+
+document.getElementById("btn1").onclick = function(){
+
+    var myScore = document.getElementById("myScore").value;
+    console.log("Nice",myScore);
+    
+
+}
+
+button.onclick = function click() {
+    console.log('Button clicked');
+
+}
+
