967e5cf3c6bbedd664d901faf2a7207451714b18
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2022-10-13 19:28:46 -0230

    Module 1 Final Project

--- input.js
+++ input.js
@@ -10,76 +10,52 @@ scoreInput.addEventListener('submit', e => {
    e.preventDefault();
    var hole = document.getElementById('hole').value;
    var score = document.getElementById('score').value;
-   console.log({hole, score});
    var cell = document.getElementById(hole);
-   console.log(cell.innerHTML)
    cell.innerHTML = score
    
-let localPersonalScore = JSON.parse(localStorage.getItem('localPersonalScore')) || []
-   console.log(localPersonalScore);
+   let localPersonalScore = JSON.parse(localStorage.getItem('localPersonalScore')) || []
    localPersonalScore [Number(hole) -1] =score
    localStorage.setItem('localPersonalScore', JSON.stringify(localPersonalScore));
-   
+   calculateSums(localPersonalScore)
     
-     
 })
-
-
-var row = 1;
-var teamScores = [["Team 1"]]
 
 function displayDetails() {
 
-   if(!Team || !H1 || !H2 || !H3 || !H4 || !H5 || !H6 || !H7 || !H8 || !H9 || !H10 || !H11 || !H12 || !H13 || !H14 || !H14 || !H15 || !H16 || !H17 || !H18) {
-      alert("Please fill all your scores");
-      return;}
+   let localPersonalScore = JSON.parse(localStorage.getItem('localPersonalScore')) || []
+   
 
-   var display = document.getElementById("display");
+      for (let index = 0; index < 18; index++) {
+         if (localPersonalScore[index]){
+            var hole = document.getElementById(`${index + 1}`)
+            hole.innerHTML = localPersonalScore[index]
 
-   let localTeamScores = JSON.parse(localStorage.getItem('localTeamScores')) || []
-   for (let Team = 0; Team < teamScores.length; Team++) {
-      var newRow = display.insertRow(row);
-         console.log(Team)
-
-      
-      console.log({localTeamScores})
-      let currentTeamScores = []
-
-      for (let index = 0; index < 19; index++) {
-         if (index==0) {
-            var Name = document.getElementById("Team").value;
-            teamScores[Team][index] =Name
-            var cell =newRow.insertCell(index);
-            cell.innerHTML = Name
-               console.log(Name)
-
-            // 
-            currentTeamScores[index] = Name
-               
-
-            }
-         else {
-            var hole = document.getElementById(`H${index}`).value
-            teamScores[Team][index] =hole
-            var cell =newRow.insertCell(index);
-            cell.innerHTML = hole
-                console.log(hole)
-            // data[`H${index}`] = hole
-
-                  currentTeamScores[index] = hole
-               
          }
-
- //  define cell IDs. (Set attribute)
-  
-    
-      }
-  localTeamScores.push(currentTeamScores)
-  localStorage.setItem('localTeamScores', JSON.stringify(localTeamScores))
-  }
-       
+         
+         }
+  calculateSums(localPersonalScore)     
 }
+function calculateSums(sums) {
+   var front = 0
+   var back = 0
+   var total = 0
+   for (let index = 0; index < 9; index++) {
+      if (sums[index]){
+         front+=Number(sums[index])
+      }
+   }
+   for (let index = 9; index < 18; index++) {
+      if (sums[index]){
+         back+=Number(sums[index])
+      }
+   }
+   total=front+back
+   document.getElementById(`front`).innerHTML=front
+   document.getElementById(`back`).innerHTML=back
+   document.getElementById(`total`).innerHTML=total
+      }
 
+displayDetails()
 // TODO
 /*
 *  Define cell IDs with setAttribute
